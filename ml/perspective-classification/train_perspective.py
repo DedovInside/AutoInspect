f378746d3653f5b7a24f3e6946934d75da3fd52f
Dmitrii Bershitskii <brshtsk@gmail.com>
2026-02-06 00:41:01 +0300

    Baseline (DagsHub)

--- ml/perspective-classification/train_perspective.py
+++ ml/perspective-classification/train_perspective.py
@@ -7,9 +7,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import os
 from collections import Counter
-import wandb
 from tqdm import tqdm
-import getpass
+
+import dagshub
+import mlflow
 
 CONFIG = {
     "project_name": "car-perspective",
@@ -19,7 +20,9 @@ CONFIG = {
     "epochs": 15,
     "learning_rate": 1e-4,
     "architecture": "resnet18",
-    "seed": 42
+    "seed": 42,
+    "dagshub_username": "brshtsk",
+    "dagshub_repo": "car-perspective"
 }
 
 
@@ -35,7 +38,7 @@ def get_data_loaders(data_dir, batch_size, img_size):
     # Аугментации
     train_transforms = transforms.Compose([
         transforms.Resize((img_size, img_size)),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2),  # Устойчивость к освещению
+        transforms.ColorJitter(brightness=0.2, contrast=0.2),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
@@ -49,7 +52,6 @@ def get_data_loaders(data_dir, batch_size, img_size):
     full_dataset_train = datasets.ImageFolder(data_dir, transform=train_transforms)
     full_dataset_val = datasets.ImageFolder(data_dir, transform=val_transforms)
 
-    # Получаем индексы для стратифицированного разбиения (чтобы во всех выборках были все классы)
     targets = full_dataset_train.targets
     train_idx, val_idx = train_test_split(
         np.arange(len(full_dataset_train)),
@@ -60,31 +62,22 @@ def get_data_loaders(data_dir, batch_size, img_size):
     )
     # ToDo: оставить меньше фото класса other (?)
 
-    # Создаем подмножества
     train_dataset = Subset(full_dataset_train, train_idx)
     val_dataset = Subset(full_dataset_val, val_idx)
 
     print(f"Train size: {len(train_dataset)}, Val size: {len(val_dataset)}")
-    print(f"Classes: {full_dataset_train.classes}")
 
-    # Балансировка
     train_targets = [targets[i] for i in train_idx]
     class_counts = Counter(train_targets)
-
-    # Вес класса
     class_weights = {cls: 1.0 / count for cls, count in class_counts.items()}
-
-    # Присваиваем вес каждому сэмплу в train датасете
     sample_weights = [class_weights[t] for t in train_targets]
 
-    # Создаем Sampler
     sampler = WeightedRandomSampler(
         weights=sample_weights,
         num_samples=len(sample_weights),
-        replacement=True  # Позволяет брать одни и те же картинки редкого класса несколько раз за эпоху
+        replacement=True
     )
 
-    # Dataloaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=sampler, num_workers=2)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
 
@@ -94,7 +87,11 @@ def get_data_loaders(data_dir, batch_size, img_size):
 def train_model():
     set_seed(CONFIG['seed'])
 
-    wandb.init(project=CONFIG["project_name"], config=CONFIG)
+    dagshub.init(repo_owner=CONFIG['dagshub_username'], repo_name=CONFIG['dagshub_repo'], mlflow=True)
+
+    mlflow.start_run()
+
+    mlflow.log_params(CONFIG)
 
     if torch.backends.mps.is_available():
         device = torch.device("mps")
@@ -111,86 +108,84 @@ def train_model():
     )
 
     model = models.resnet18(weights='DEFAULT')
-
-    # Заменяем последний слой
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, num_classes)
-
     model = model.to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=CONFIG['learning_rate'])
 
-    wandb.watch(model, log="all", log_freq=10)
-
     best_acc = 0.0
 
     # Train loop
-    for epoch in range(CONFIG['epochs']):
-        print(f"\nExample {epoch + 1}/{CONFIG['epochs']}")
+    try:
+        for epoch in range(CONFIG['epochs']):
+            print(f"\nEpoch {epoch + 1}/{CONFIG['epochs']}")
 
-        model.train()
-        train_loss = 0.0
-        train_corrects = 0
+            model.train()
+            train_loss = 0.0
+            train_corrects = 0
 
-        # Tqdm для красивой полоски прогресса
-        for inputs, labels in tqdm(train_loader, desc="Training"):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            optimizer.zero_grad()
-
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-            loss = criterion(outputs, labels)
-
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item() * inputs.size(0)
-            train_corrects += torch.sum(preds == labels.data)
-
-        epoch_train_loss = train_loss / len(train_loader.dataset)
-        epoch_train_acc = train_corrects.double() / len(train_loader.dataset)
-
-        # Val
-        model.eval()
-        val_loss = 0.0
-        val_corrects = 0
-
-        with torch.no_grad():
-            for inputs, labels in val_loader:
+            for inputs, labels in tqdm(train_loader, desc="Training"):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
+
+                optimizer.zero_grad()
 
                 outputs = model(inputs)
                 _, preds = torch.max(outputs, 1)
                 loss = criterion(outputs, labels)
 
-                val_loss += loss.item() * inputs.size(0)
-                val_corrects += torch.sum(preds == labels.data)
+                loss.backward()
+                optimizer.step()
 
-        epoch_val_loss = val_loss / len(val_loader.dataset)
-        epoch_val_acc = val_corrects.double() / len(val_loader.dataset)
+                train_loss += loss.item() * inputs.size(0)
+                train_corrects += torch.sum(preds == labels.data)
 
-        print(f"Train Loss: {epoch_train_loss:.4f} Acc: {epoch_train_acc:.4f}")
-        print(f"Val Loss: {epoch_val_loss:.4f} Acc: {epoch_val_acc:.4f}")
+            epoch_train_loss = train_loss / len(train_loader.dataset)
+            epoch_train_acc = train_corrects.double() / len(train_loader.dataset)
 
-        wandb.log({
-            "epoch": epoch + 1,
-            "train_loss": epoch_train_loss,
-            "train_acc": epoch_train_acc,
-            "val_loss": epoch_val_loss,
-            "val_acc": epoch_val_acc
-        })
+            # Val
+            model.eval()
+            val_loss = 0.0
+            val_corrects = 0
 
-        if epoch_val_acc > best_acc:
-            best_acc = epoch_val_acc
-            torch.save(model.state_dict(), "best_car_view_model.pth")
-            print("Model saved!")
+            with torch.no_grad():
+                for inputs, labels in val_loader:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
 
-    print(f"Обучение завершено. Лучшая точность: {best_acc:.4f}")
-    wandb.finish()
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    val_loss += loss.item() * inputs.size(0)
+                    val_corrects += torch.sum(preds == labels.data)
+
+            epoch_val_loss = val_loss / len(val_loader.dataset)
+            epoch_val_acc = val_corrects.double() / len(val_loader.dataset)
+
+            print(f"Train Loss: {epoch_train_loss:.4f} Acc: {epoch_train_acc:.4f}")
+            print(f"Val Loss: {epoch_val_loss:.4f} Acc: {epoch_val_acc:.4f}")
+
+            mlflow.log_metrics({
+                "train_loss": float(epoch_train_loss),
+                "train_acc": float(epoch_train_acc),
+                "val_loss": float(epoch_val_loss),
+                "val_acc": float(epoch_val_acc)
+            }, step=epoch + 1)
+
+            if epoch_val_acc > best_acc:
+                best_acc = epoch_val_acc
+                # Сохраняем локально, как и было
+                torch.save(model.state_dict(), "best_car_view_model.pth")
+                print("Model saved locally!")
+
+                mlflow.log_artifact("best_car_view_model.pth")
+
+    finally:
+        print(f"Обучение завершено. Лучшая точность: {best_acc:.4f}")
+        mlflow.end_run()
 
 
 if __name__ == '__main__':
