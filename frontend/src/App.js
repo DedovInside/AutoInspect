4cd7d62d774a2c9fa9e04797a60cc356e3a8567f
Полина Гусева <polinaguseva@MacBook-Air-Polina-2.local>
2026-02-13 20:24:01 +0300

    Initial commit: add frontend React application

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AutoInspect</h1>
+      <p>Веб-сервис автоматического анализа повреждений кузова автомобилей</p>
+    </div>
+  );
+}
+
+export default App;
